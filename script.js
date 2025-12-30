c5295052647e4333d23df25c8a0d7cac5181b346
Revan0809 <redupuga@gitam.in>
2025-12-30 21:07:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function register() {
+    alert("Registration opens soon! Stay tuned 🚀");
+}
+
+function showSchedule() {
+    alert(
+        "Schedule:\n\n" +
+        "9:00 AM - Inauguration\n" +
+        "10:00 AM - Hackathon\n" +
+        "1:00 PM - Lunch\n" +
+        "3:00 PM - Workshops\n" +
+        "6:00 PM - Closing Ceremony"
+    );
+}
+
+function contact() {
+    alert("Contact us at: techfest@gitam.edu");
+}
